b7da7b62bdea176550b5924d0c7f86d6ed59d09f
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-02-17 15:48:35 +0100

    Modifica cartella lezione 2

---
+++ Lezione_02/esercizi_dato_operatori/esercizio1-4.py
@@ -0,0 +1,12 @@
+''' 1-4
+    Si scriva un programma che dato un intero di quattro cifre, per esempio 2024, utilizzando gli
+    opportuni operatori, lo si visualizzi, una cifra per riga:'''
+
+
+x: int = 2025
+mylist= [2,0,2,5]
+
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
---
+++ Lezione_02/esercizi_dato_operatori/esercizio1-5.py
@@ -0,0 +1,11 @@
+''' 1-5
+    Si scriva un programma che converta la temperatura da Fahrenheit a Celsius utilizzando la formula
+    gradiCelsius=5∗(gradiFahrenheit−32)/9
+    Si inizializzi una temperatura espressa in gradi Fahrenheit con un numero intero.
+    La temperatura deve essere convertita e visualizzata in gradi Celsius con un numero in virgola mobile
+    con una precisione di un decimo di grado.'''
+
+gradi_fahrenheit: int = 72
+gradi_Celsius = 5*(gradi_fahrenheit - 32)/9
+
+print(gradi_fahrenheit)
