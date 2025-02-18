7a9f4d5d8669db295d015bc724fb4b526c837c1f
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-02-18 17:44:42 +0100

    esercizio 3-3

--- Lezione_02/esercizi_dato_operatori_collection/esercizio3-3.py
+++ Lezione_02/esercizi_dato_operatori_collection/esercizio3-3.py
@@ -3,6 +3,7 @@ Your Own List: Think of your favorite mode of transportation, such as a motorcyc
 and make a list that stores several examples. Use your list to print a series of statements about these items,
 such as “I would like to own a Honda motorcycle.”'''
 
-myfavoritelist:list = ["motocicletta Honda", "Macchina Ducia Duster"]
+myfavoritelist:list = ["Motocicletta Honda", "Macchina Ducia Duster", "Macchina Ferrari", "Motocicletta Ducati", "Macchina Lamborghini"]
 
-print(myfavoritelist)
+for items in myfavoritelist:
+        print(f"Mi piacerebbe avere una", items)
