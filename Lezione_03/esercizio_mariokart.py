396cf02554e63c668be15bc203888c08c71d7014
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-02-18 18:12:53 +0100

    esercizio lezioe 3

---
+++ Lezione_03/esercizio_mariokart.py
@@ -0,0 +1,14 @@
+
+position:int= int(input("Insert finishing position: "))
+
+if position == 1:
+    print(f"{position}st!")
+
+elif position == 2:
+    print(f"{position}nd!")
+
+elif position == 3:
+    print(f"{position}rd!")
+
+else:
+    print(f"{position}th!")
