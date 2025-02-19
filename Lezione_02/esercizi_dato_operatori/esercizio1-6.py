ae9ae20ca7b855e977d70c333f8d48d4c661d1c9
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-02-19 18:09:01 +0100

    modifica esercizio1-6

--- Lezione_02/esercizi_dato_operatori/esercizio1-6.py
+++ Lezione_02/esercizi_dato_operatori/esercizio1-6.py
@@ -7,17 +7,15 @@
     "Salmone":20.20 euro, "Patatine Fritte":5.50 euro, "Patate al forno":5.50 euro, "Verdura del giorno":7.00 euro, 
     "Cheesecake":6.00 euro, "Tiramisu":6.00 euro, "Focaccia con Nutella":6.00 euro, "Coca Cola":3.50 euro, "Acqua":1.50 euro, "Vino":5.00 euro'''
 
-ITS_Bakery_Menù:dict = {"Pizza":9.00, "Pasta":10.50, "Zuppa":7.00, "Hamburger": 15.50, "Cotoletta": 10.00, "Salmone": 20.20, "Patatine_Fritte": 5.50, "Patate_al_forno": 5.50,
-"Verdura_del_giorno": 7.00, "Cheesecake": 6.00, "Tiramisu": 6.00, "Focaccia_con_Nutella": 6.00, "Coca_Cola": 3.50, "Acqua": 1.50, "Vino": 5.00}
+ITS_Bakery_Menù:dict = {"Pizza":9.00, "Pasta":10.50, "Zuppa":7.00, "Hamburger":15.50, "Cotoletta":10.00, "Salmone":20.20, "Patatine_Fritte":5.50, "Patate_al_forno":5.50,
+"Verdura_del_giorno":7.00, "Cheesecake":6.00, "Tiramisu":6.00, "Focaccia_con_Nutella":6.00, "Coca_Cola":3.50, "Acqua":1.50, "Vino":5.00}
 
 totale:float = 0
-ordine:dict={"Pizza":9.00, "Patatine_Fritte": 5.50, "Cheesecake": 6.00, "Coca_Cola": 3.50}
+ordine:dict={"Pizza":9.00, "Patatine_Fritte":5.50, "Cheesecake":6.00, "Coca_Cola":3.50}
 
 totale += ordine["Pizza"]
 totale += ordine["Patatine_Fritte"]
 totale += ordine["Cheesecake"]
 totale += ordine["Coca_Cola"]
 
-print(totale)
-
-
+print(f"Il totale è €{totale:.2f}")
