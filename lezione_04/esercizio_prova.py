4be2f3e2790fa17404c0ac95ac93e90155dbf1f3
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-04 17:47:41 +0100

    Lezione 4 esercizio di prova per vedere le funzioni

---
+++ lezione_04/esercizio_prova.py
@@ -0,0 +1,19 @@
+'''Fare tre diverse somme, sotto indicate. Esempio per spiegare le Funzioni.'''
+
+# 1 Sum of integers from 1 to 10
+sum:int = 0
+for i in range(1, 11):
+    sum += i
+print("Sum from 1 to 10 is:", sum)
+
+# 2 Sum of integers from 20 to 37
+sum:int = 0
+for i in range(20, 38):
+    sum += i
+print("Sum from 20 to 37 is:", sum)
+
+# 3 Sum of integers from 35 to 49
+sum:int = 0
+for i in range(35, 50):
+    sum += i
+print("Sum from 35 to 49 is:", sum)
