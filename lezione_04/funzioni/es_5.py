518d135ca82b23ebcb1302a99730d8c1aa68ae9b
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-05 19:09:05 +0100

    esercizi 8-1 8-2 8-3 lezione 4

--- lezione_04/funzioni/es_5.py
+++ lezione_04/funzioni/es_5.py
@@ -12,6 +12,10 @@ def add_one(numero:int):
 
 list:list = [1, 2, 3, 4, 5, 6, 7, 8]
 
+#per scrivere una sequenza di numeri da a 
+
+#list2:list = [i for i in range(1, 11)]
+
 def add_one_to_list(list):
     new_list = []
     for numero in list:
