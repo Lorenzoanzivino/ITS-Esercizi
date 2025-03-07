d32c3a024b8c7dde7c02164407aae2ef014586af
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-07 18:06:41 +0100

    modifica

--- lezione_04/funzioni/es_8-7.py
+++ lezione_04/funzioni/es_8-7.py
@@ -1,7 +1,7 @@
 '''8-7. Album: scrivi una funzione chiamata make-album() che costruisce un dizionario che descrive un album musicale. La funzione dovrebbe assumere un nome d'artista e un titolo dell'album, e dovrebbe restituire un dizionario contenente queste due informazioni. Usa la funzione per creare tre dizionari che rappresentano diversi album. Stampare ogni valore di ritorno per mostrare che i dizionari memorizzano correttamente le informazioni dell'album. Usare Nessuno per aggiungere un parametro opzionale per make-album() che consente di memorizzare il numero di brani su un album. Se la riga di chiamata include un valore per il numero di brani, aggiungere tale valore al dizionario dell'album. Effettua almeno una nuova chiamata di funzione che include il numero di canzoni su un album.'''
 
 
-def make_album(name:str, album:str, songs_number:int = None) ->dict:
+def make_album(name:str, album:str, songs_number:int = None) -> dict:  # Definire il TIPO di una funzione quando ho un ritorno "-> NONE" quando non restituisce niente
     # sostituisco None con str "Unknow"
     if songs_number is None:
         songs_number = "Unknown"
