3e7bf2df076a3c40fa97428aa895fae8f6e12e81
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-07 17:59:39 +0100

    modifiche

--- lezione_04/funzioni/es_8-6.py
+++ lezione_04/funzioni/es_8-6.py
@@ -1,8 +1,11 @@
 '''8-6. Nomi della città: scrivi una funzione chiamata city-country() che prende il nome di una città e del suo paese. La funzione dovrebbe restituire una stringa formattata in questo modo: "Santiago, Cile". Chiama la tua funzione con almeno tre coppie di città-paese e stampa i valori che vengono restituiti.'''
 
-def city_country(citta:str, paese:str):
-    print(citta, paese, sep=', ')
+def city_country(city:str,country:str):
+    formatted:str = f"{city.capitalize()}, {country.capitalize()}"
 
-city_country("Roma", "Italia")
-city_country("Parigi", "Francia")
-city_country("Santiago", "Cile")
+    return formatted
+
+
+print(city_country("roma","italia"))
+print(city_country("los angeles", "california"))
+print(city_country("stockholm", "sweden"))
--- lezione_04/funzioni/es_8-7.py
+++ lezione_04/funzioni/es_8-7.py
@@ -1,9 +1,13 @@
 '''8-7. Album: scrivi una funzione chiamata make-album() che costruisce un dizionario che descrive un album musicale. La funzione dovrebbe assumere un nome d'artista e un titolo dell'album, e dovrebbe restituire un dizionario contenente queste due informazioni. Usa la funzione per creare tre dizionari che rappresentano diversi album. Stampare ogni valore di ritorno per mostrare che i dizionari memorizzano correttamente le informazioni dell'album. Usare Nessuno per aggiungere un parametro opzionale per make-album() che consente di memorizzare il numero di brani su un album. Se la riga di chiamata include un valore per il numero di brani, aggiungere tale valore al dizionario dell'album. Effettua almeno una nuova chiamata di funzione che include il numero di canzoni su un album.'''
 
 
-def make_album(name: str, album: str, songs_number: int = "Unknown"):
+def make_album(name:str, album:str, songs_number:int = None):
+    # sostituisco None con str "Unknow"
+    if songs_number is None:
+        songs_number = "Unknown"
     # Restituisce il dizionario contenente le informazioni sull'album
     return {"name": name, "album": album, "songs_number": songs_number}
+
 
 # Lista di album
 albums = [
