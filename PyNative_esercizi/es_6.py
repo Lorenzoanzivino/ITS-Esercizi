53992302c99db2e641125b01329bd55cb9b16d83
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-13 10:45:47 +0100

    es_ 6 aggiunta di cicli

--- PyNative_esercizi/es_6.py
+++ PyNative_esercizi/es_6.py
@@ -1,5 +1,6 @@
 '''Es 6 Scrivere un programma per creare una funzione ricorsiva per calcolare la somma dei numeri da 0 a 10. '''
 
+# Senza Ciclo
 def ricorsiva(a: int) -> int:
         return (a*(a+1))/2
 
@@ -8,6 +9,7 @@ result = ricorsiva(0)
 print(result)
 
 
+# Ciclo While
 def somma_con_ciclo_while(a: int) -> int:
     somma = 0
     i = 0
@@ -18,3 +20,16 @@ def somma_con_ciclo_while(a: int) -> int:
 
 result = somma_con_ciclo_while(10)
 print(result)
+
+
+# Ciclo For
+def sum(a:int):
+    mysum=0
+    for i in range(a+1):
+        mysum+=i
+        print(mysum)
+
+    return mysum
+
+print(sum(10))
+
