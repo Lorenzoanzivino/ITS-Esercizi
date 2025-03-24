628f34aa8efaaf1d70673b5bcc981643d004234f
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-24 13:01:05 +0100

    es 6

---
+++ Lezione_04/esercizi_opzionali/es_6.py
@@ -0,0 +1,8 @@
+'''6. Generatore di password:
+Crea una funzione che genera una password casuale con una lunghezza specificata e i tipi di caratteri desiderati (lettere minuscole, lettere maiuscole, numeri, simboli).
+Permetti all'utente di specificare la lunghezza della password e i tipi di caratteri desiderati.
+Genera e restituisci una password casuale che soddisfi i criteri dell'utente.'''
+
+import random
+
+def password() -> None:
