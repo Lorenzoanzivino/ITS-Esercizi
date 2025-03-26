2eb679021b1981ff814b5101a62789b49327ed20
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-26 09:17:30 +0100

    esercizio 8 da iniziare

---
+++ Lezione_04/esercizi_opzionali/es_8.py
@@ -0,0 +1,6 @@
+''' 8. Simulatore di Bancomat: 
+Crea una funzione che simuli un bancomat.
+Inizializza un conto con un saldo iniziale.
+Consenti all'utente di eseguire transazioni come deposito, prelievo e verifica del saldo.
+Valida le transazioni contro il saldo del conto e i fondi disponibili.
+Fornisci un feedback appropriato all'utente per ogni transazione.'''
