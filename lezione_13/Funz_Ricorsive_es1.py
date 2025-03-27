16385a5cded9dfe8ebbac8dfb74f49096e659f3e
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-27 13:49:38 +0100

    modifica esempio  2

--- lezione_13/Funz_Ricorsive_es1.py
+++ lezione_13/Funz_Ricorsive_es1.py
@@ -54,3 +54,23 @@ print(f"\n{sum(5)}")'''
 print(recursive_sum(5))'''
 
 # Esempio 2
+def recursiveSumInRange(a:int, b:int) -> int:
+    # if a > b, swap values of a and b
+    if a > b:
+        # define a temporary variable called temp, containing value of a
+        temp:int = a
+
+        # swap values of a and b
+        a = b
+        b = temp # now b = a
+
+    # if b = a, the recursive process must be stopped
+    if b == a:
+        return int(a)
+    
+    # otherwise, compute the sum recursively
+    else:
+        return int(b + recursiveSumInRange(a, b-1))
+    
+# calling sumInRange function for a=5, b=10
+print(recursiveSumInRange(5,10))
