ef4ab825f155403d578226b85c4ab4be6a01d8d0
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2025-03-27 13:20:25 +0100

    aggiunta esempi funzione ricorsiva

--- lezione_13/Funz_Ricorsive_es1.py
+++ lezione_13/Funz_Ricorsive_es1.py
@@ -1,1 +1,54 @@
+'''def recursive_countdown(n:int) -> None:
 
+    # Step 1 "n is negative"
+    if n < 0 :
+        print("Error! Inserted number is negative!")
+
+    # Step 2 "n = 0 si ferma la ricorsione"
+    elif n == 0:
+        print(0)
+
+    # Se n > 0 stampa n incrementato finchè n non corrisponderà a uno dei due casi di blocco (n<0 oppure n==n)
+    else:
+        print(n)
+        recursive_countdown(n-1)
+
+recursive_countdown(5)'''
+
+'''def sum(n:int) -> int:
+    # n is negative
+    if n < 0:
+        print("\nError! Inserted number is negative!")
+        return None
+    # n is positive
+    else:
+        # define a sum
+        sum:int = 0
+        # compute sum until n >= 0
+        while n:
+            sum = sum + n
+            n = n-1
+    # return sum as int
+    return int(sum)
+
+# calling sum function for n = -5
+print(sum(-5))
+# calling sum function for n = 5
+print(f"\n{sum(5)}")'''
+
+
+def recursive_sum(n:int) -> int:
+    # n is negative
+    if n < 0:
+        print("Error! Inserted number is negative!")
+        return None
+
+    # if n = 0 recursive process must be stopped
+    elif n == 0:
+        return 0
+    
+    # if n is positive, compute recursive sum
+    else:
+        return int(n + recursive_sum(n-1))
+    
+print(recursive_sum(5))
